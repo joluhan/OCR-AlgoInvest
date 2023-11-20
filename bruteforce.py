53b91bb92ceba89c49522e93acc02bdce9e5c1a0
joluhan <135083872+joluhan@users.noreply.github.com>
2023-11-20 12:18:15 +0700

    Function to read stock data from a CSV file

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,15 @@
 import csv
+
+# Function to read stock data from a CSV file
+def read_stock_data_from_csv(file_path):
+    stocks = []
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
+        stock_reader = csv.reader(csvfile, delimiter=',')
+        next(stock_reader)  # Skipping the header row
+        for row in stock_reader:
+            name = row[0]
+            cost = float(row[1])
+            profit_percentage = float(row[2].replace('%', ''))
+            profit = cost * profit_percentage / 100
+            stocks.append((name, cost, profit))
+    return stocks
