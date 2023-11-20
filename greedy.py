d0c9705dc304546e7501ba2e6d74f1e63179bf7f
joluhan <135083872+joluhan@users.noreply.github.com>
2023-11-20 12:22:11 +0700

    Function that optimized investment algorithm using a greedy approach

--- greedy.py
+++ greedy.py
@@ -13,3 +13,21 @@ def read_stock_data_from_csv(file_path):
             profit = cost * profit_percentage / 100
             stocks.append((name, cost, profit))
     return stocks
+
+# Optimized investment algorithm using a greedy approach
+def optimized_investment(stocks, max_investment=500):
+    """Sorts and returns the best combination of stocks and the total profit"""
+    # 
+    sorted_stocks = sorted(stocks, key=lambda x: x[2]/x[1], reverse=True)
+
+    total_cost = 0
+    total_profit = 0
+    selected_stocks = []
+
+    for stock in sorted_stocks:
+        if total_cost + stock[1] <= max_investment:
+            selected_stocks.append(stock)
+            total_cost += stock[1]
+            total_profit += stock[2]
+
+    return selected_stocks, total_profit
